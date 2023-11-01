3a0f2cc394f949ec8416d1e034a278271e66f3a4
iliko13 <107374131+iliko13@users.noreply.github.com>
2023-11-01 16:10:41 +0400

    Second Commit

--- src/Pages/Store.tsx
+++ src/Pages/Store.tsx
@@ -2,6 +2,19 @@ import image1 from "../assets/image-1.jpeg";
 import image2 from "../assets/image-2.jpeg";
 import image3 from "../assets/image-3.jpeg";
 import image4 from "../assets/image-4.jpeg";
+import image5 from "../assets/image-5.jpeg";
+import image6 from "../assets/image-6.jpeg";
+import image7 from "../assets/image-7.jpeg";
+import image8 from "../assets/image-8.jpeg";
+import image9 from "../assets/image-9.jpeg";
+import image10 from "../assets/image-10.jpeg";
+import image11 from "../assets/image-11.jpeg";
+import image12 from "../assets/image-12.jpeg";
+import image13 from "../assets/image-13.jpeg";
+import image14 from "../assets/image-14.jpeg";
+import image15 from "../assets/image-15.jpeg";
+import image16 from "../assets/image-16.jpeg";
+import image17 from "../assets/image-17.jpeg";
 import StoreItem from "../components/StoreItem";
 
 type Item = {
@@ -32,9 +45,87 @@ export const storeItems: Item[] = [
   },
   {
     id: 4,
-    name: "Tamari",
+    name: "Elithabeth",
     price: 150,
     image: image4,
+  },
+  {
+    id: 5,
+    name: "Davit",
+    price: 140,
+    image: image5,
+  },
+  {
+    id: 6,
+    name: "Tamari",
+    price: 150,
+    image: image6,
+  },
+  {
+    id: 7,
+    name: "Tamari",
+    price: 150,
+    image: image7,
+  },
+  {
+    id: 8,
+    name: "Elothabeth",
+    price: 140,
+    image: image8,
+  },
+  {
+    id: 9,
+    name: "Elithabeth",
+    price: 200,
+    image: image9,
+  },
+  {
+    id: 10,
+    name: "Davit",
+    price: 170,
+    image: image10,
+  },
+  {
+    id: 11,
+    name: "Tamari",
+    price: 190,
+    image: image11,
+  },
+  {
+    id: 12,
+    name: "Davit",
+    price: 130,
+    image: image12,
+  },
+  {
+    id: 13,
+    name: "Tamari",
+    price: 350,
+    image: image13,
+  },
+  {
+    id: 14,
+    name: "Giorgi",
+    price: 2000,
+    image: image14,
+  },
+  {
+    id: 15,
+    name: "Tamari",
+    price: 250,
+    image: image15,
+  },
+  {
+    id: 16,
+    name: "Giorgi",
+    price: 170,
+    image: image16,
+  },
+  {
+    id: 17,
+    name: "Tamari",
+    price: 100,
+    image: image17,
   },
 ];
 
